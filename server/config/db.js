93e0fa65e27513080a867a8aa0fc91c6f111187a
Annie16GG <sami-guzman13@hotmail.com>
2024-08-28 14:14:24 -0600

    vercel2-2

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: '1234',
   database: 'share_analytics'
