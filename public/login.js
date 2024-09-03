a030c8a7ef36e4ad604b419494f6eca3b660f512
Annie16GG <sami-guzman13@hotmail.com>
2024-09-03 16:48:07 -0600

    Cambios 03/09-v4

--- public/login.js
+++ public/login.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async (event) =>
   const password = document.getElementById('password').value;
   
   try {
-    const response = await fetch('/api/api/auth/login', {
+    const response = await fetch('/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
