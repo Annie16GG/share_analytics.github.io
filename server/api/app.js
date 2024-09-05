2fa190fc9148b20a7afaf5d9377b6151ea3d433d
Annie16GG <sami-guzman13@hotmail.com>
2024-09-05 10:28:48 -0600

    Cambios 05/09-v1

--- server/api/app.js
+++ server/api/app.js
@@ -43,3 +43,63 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// const cors = require('cors');
+// const fs = require('fs');
+// const http = require('http');
+// const https = require('https');
+// const crypto = require('crypto');
+// const url = require('url');
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static('public'));
+
+// // Usa el router de boldbiRoutes
+// app.use('/api', boldbiRoutes);
+// const appconfig = JSON.parse(fs.readFileSync('./server/api/embedConfig.json'));
+// const embedSecret = appconfig.EmbedSecret;
+// const configjson = {
+//   "DashboardId": appconfig.DashboardId,
+//   "ServerUrl": appconfig.ServerUrl,
+//   "SiteIdentifier": appconfig.SiteIdentifier,
+//   "Environment": appconfig.Environment,
+//   "EmbedType": appconfig.EmbedType
+// };
+
+// const userEmail = appconfig.UserEmail;
+// console.log(userEmail);
+
+// app.post('/embeddetail/get', function (req, response) {
+//   let embedQuerString = req.body.embedQuerString;
+//   let dashboardServerApiUrl = req.body.dashboardServerApiUrl;
+
+//   embedQuerString += "&embed_user_email=" + userEmail;
+//   embedQuerString += "&embed_server_timestamp=" + Math.round((new Date()).getTime() / 1000);
+//   const embedSignature = "&embed_signature=" + GetSignatureUrl(embedQuerString);
+//   const embedDetailsUrl = "/embed/authorize?" + embedQuerString + embedSignature;
+
+//   const serverProtocol = url.parse(dashboardServerApiUrl).protocol == 'https:' ? https : http;
+//   serverProtocol.get(dashboardServerApiUrl + embedDetailsUrl, function (res) {
+//     let str = '';
+//     res.on('data', function (chunk) {
+//       str += chunk;
+//     });
+//     res.on('end', function () {
+//       response.send(str);
+//     });
+//   });
+// });
+
+// function GetSignatureUrl(queryString) {
+//   const keyBytes = Buffer.from(embedSecret);
+//   const hmac = crypto.createHmac('sha256', keyBytes);
+//   const data = hmac.update(queryString);
+//   return data.digest().toString('base64');
+// }
+
+// const server = app.listen(8080, 'https://share-analytics.vercel.app/', function () {
+//   const host = server.address().address;
+//   const port = server.address().port;
+//   console.log("Example app listening at http://%s:%s", host, port);
+// });
--- server/api/embed.js
+++ server/api/embed.js
@@ -1,75 +1,72 @@
+const cors = require('cors');
 const fs = require('fs');
-const crypto = require('crypto');
-const url = require('url');
 const http = require('http');
 const https = require('https');
+const crypto = require('crypto');
+const url = require('url');
 const express = require('express');
-const cors = require('cors');  // Importa CORS
-const boldbiRoutes = require('./server/routes/boldbiRoutes');
+const boldbiRoutes = require('./server/routes/boldbiRoutes');  // Asegúrate de ajustar la ruta si es necesario.
 
-// Inicializa la aplicación Express
 const app = express();
 
-// Configura CORS
-const corsOptions = {
-  origin: 'https://share-analytics.vercel.app',  // Tu dominio en Vercel
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// Configurar CORS
+app.use(cors({
+  origin: 'https://share-analytics.vercel.app', // El dominio desde el que harás la petición.
+  methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-};
+}));
 
-app.use(cors(corsOptions));  // Aplica la configuración de CORS
-
+// Middleware
 app.use(express.json());
 app.use(express.static('public'));
+
+// Usa el router de boldbiRoutes
 app.use('/api', boldbiRoutes);
 
-// Exportar la función handler para que funcione como una API en Vercel
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const appconfig = JSON.parse(fs.readFileSync('/embedConfig.json'));
-    const embedSecret = appconfig.EmbedSecret;
-    const configjson = {
-      "DashboardId": appconfig.DashboardId,
-      "ServerUrl": appconfig.ServerUrl,
-      "SiteIdentifier": appconfig.SiteIdentifier,
-      "Environment": appconfig.Environment,
-      "EmbedType": appconfig.EmbedType
-    };
+// Configuración de BoldBI
+const appconfig = JSON.parse(fs.readFileSync('./server/api/embedConfig.json'));
+const embedSecret = appconfig.EmbedSecret;
+const configjson = {
+  "DashboardId": appconfig.DashboardId,
+  "ServerUrl": appconfig.ServerUrl,
+  "SiteIdentifier": appconfig.SiteIdentifier,
+  "Environment": appconfig.Environment,
+  "EmbedType": appconfig.EmbedType
+};
 
-    const userEmail = appconfig.UserEmail;
-    console.log(userEmail);
+const userEmail = appconfig.UserEmail;
 
-    let embedQuerString = req.body.embedQuerString;
-    let dashboardServerApiUrl = req.body.dashboardServerApiUrl;
+// Ruta para obtener los detalles del embed
+app.post('/embeddetail/get', function (req, res) {
+  let embedQuerString = req.body.embedQuerString;
+  let dashboardServerApiUrl = req.body.dashboardServerApiUrl;
 
-    embedQuerString += "&embed_user_email=" + userEmail;
-    embedQuerString += "&embed_server_timestamp=" + Math.round((new Date()).getTime() / 1000);
-    const embedSignature = "&embed_signature=" + GetSignatureUrl(embedQuerString, embedSecret);
-    const embedDetailsUrl = "/embed/authorize?" + embedQuerString + embedSignature;
+  embedQuerString += "&embed_user_email=" + userEmail;
+  embedQuerString += "&embed_server_timestamp=" + Math.round((new Date()).getTime() / 1000);
+  const embedSignature = "&embed_signature=" + GetSignatureUrl(embedQuerString);
+  const embedDetailsUrl = "/embed/authorize?" + embedQuerString + embedSignature;
 
-    const serverProtocol = url.parse(dashboardServerApiUrl).protocol === 'https:' ? https : http;
-    serverProtocol.get(dashboardServerApiUrl + embedDetailsUrl, function (response) {
-      let str = '';
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-      response.on('end', function () {
-        res.status(200).send(str);
-      });
-    }).on('error', (e) => {
-      res.status(500).send(e.message);
+  const serverProtocol = url.parse(dashboardServerApiUrl).protocol === 'https:' ? https : http;
+  serverProtocol.get(dashboardServerApiUrl + embedDetailsUrl, function (response) {
+    let str = '';
+    response.on('data', function (chunk) {
+      str += chunk;
     });
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
+    response.on('end', function () {
+      res.status(200).send(str);
+    });
+  }).on('error', (e) => {
+    res.status(500).send(e.message);
+  });
+});
 
-function GetSignatureUrl(queryString, embedSecret) {
+// Función para generar la firma del embed
+function GetSignatureUrl(queryString) {
   const keyBytes = Buffer.from(embedSecret);
   const hmac = crypto.createHmac('sha256', keyBytes);
   const data = hmac.update(queryString);
   return data.digest().toString('base64');
 }
+
+// Exportar la función handler para que Vercel la utilice
+module.exports = app;
